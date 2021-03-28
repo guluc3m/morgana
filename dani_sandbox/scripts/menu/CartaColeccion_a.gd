b71a594afed38ccbae56dbfb38d705ad562168f5
Patataman <shas_arroyo@hotmail.com>
2021-03-28 11:47:35 +0200

    añadir y quitar del mazo (early)

---
+++ dani_sandbox/scripts/menu/CartaColeccion_a.gd
@@ -0,0 +1,20 @@
+extends Control
+
+signal a_mazo;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_CartaColeccion_gui_input(event):
+	""" Evento para saber cuándo el usuario hace click o hace click derecho en la carta
+	"""
+	if Input.is_action_just_released("mouse_left"):
+		# Idealmente aquí se pasan los argumentos necesarios para que podamos
+		# identificar la carta y añadirla correctamente.
+		emit_signal("a_mazo", "pepe");
+		
+	if Input.is_action_just_released("mouse_right"):
+		# TODO puede que haya que definirlo en los ajustes del proyecto
+		print(event)
---
+++ dani_sandbox/scripts/menu/CartaMazo_a.gd
@@ -0,0 +1,16 @@
+extends Control
+
+signal a_coleccion;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_CartaMazo_gui_input(event):
+	""" Evento para saber cuándo el usuario hace click y así eliminarla (visualmente)
+		del mazo.
+	"""
+	if Input.is_action_just_released("mouse_left"):
+		queue_free();
+		emit_signal("a_coleccion", "pepe");
